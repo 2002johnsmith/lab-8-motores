ce60356439663303d2c3414e6129f413da38f771
John Smith <john.ayudante@tecsup.edu.pe>
2025-05-08 00:43:52 -0500

    G movement done

---
+++ Assets/Scripts/GameDos/CharacterController3D.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController3D : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] private float Speed;
+    [SerializeField] private float JumpForce;
+    private Vector3 Movement; 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(Movement.x * Speed, rb.linearVelocity.y, Movement.z*Speed); 
+    }
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Debug.Log("MOVIMIENTO");
+        Vector2 Direction=context.ReadValue<Vector2>();
+        Movement = new Vector3(Direction.x, 0, Direction.y);
+    }
+    public void OnJump()
+    {
+
+    }
+    public void ApplyPhysics()
+    {
+
+    }
+}
